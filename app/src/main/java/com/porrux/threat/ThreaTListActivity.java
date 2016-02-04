e49540734c2d565204b6940cbaf1ba9ff54043f8
ojbruce <olivia.j.bruce@gmail.com>
2016-02-04 13:59:29 +0100

    Initial commit

---
+++ app/src/main/java/com/porrux/threat/ThreaTListActivity.java
@@ -0,0 +1,13 @@
+package com.porrux.threat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreaTListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_threa_tlist);
+    }
+}
